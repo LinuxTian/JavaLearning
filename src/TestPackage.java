03b65e5a760632d240246027da290c210ff5ea92
tzh <tianzhihao1019@sina.com>
2019-05-18 14:36:18 +0800

    New commit for learning package and import
    [+] add new file to test package and import function

---
+++ src/TestPackage.java
@@ -0,0 +1,9 @@
+import packageTest.AddStudent;
+
+public class TestPackage {
+    public static void main(String[] args){
+        AddStudent as = new AddStudent();
+        as.add();
+        System.out.println(as);
+    }
+}
---
+++ src/packageTest/AddStudent.java
@@ -0,0 +1,7 @@
+package packageTest;
+
+public class AddStudent {
+    public void add(){
+        System.out.println("添加学生");
+    }
+}
